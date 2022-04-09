281a6f740d75bf45a6da8e31435ba721b581cbf3
Jean Joel Kapula <jeanjoelkapula@gmail.com>
2022-04-09 14:58:20 +0200

    Add user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    followers = models.ManyToManyField('self', related_name = 'followees')
+
+    def __str__(self):
+        return f"{self.username}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "followers": [{"id": u.id, "username": u.username} for u in self.followers.all()],
+            "followees": [{"id": u.id, "username": u.username}  for u in self.followees.all()]
+        }
